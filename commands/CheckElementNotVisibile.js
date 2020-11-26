72bc613e55130dfb356e59ba7e01774945f3ac60
Michał <michal.zajkowski.94@gmail.com>
2020-11-26 08:55:10 +0100

    clean code

--- commands/CheckElementNotVisibile.js
+++
@@ -1,4 +0,0 @@
-module.exports.command = function (selector, timeout) {
-  this.waitForElementNotVisible(selector, timeout, 'Element with selector ' +
-    selector + ' is visible with timeout ' + timeout + " milliseconds")
-}
